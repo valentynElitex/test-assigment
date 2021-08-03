72a9253afbfa1a9ea4fd04f0b85062b8f4b51d5f
Valentyn Vovk <vvovk@elitex.systems>
2021-08-03 14:18:27 +0300

    updated styles

--- src/Components/VacancyItem/VacancyItem.js
+++ src/Components/VacancyItem/VacancyItem.js
@@ -3,16 +3,23 @@ import PropTypes from 'prop-types';
 
 import './VacancyItem.css';
 
-export const VacancyItem = ({ jobTitle, companyName, shortDesc, postedDate, OBJurl }) => (
-  <div
-    onClick={() => window.open(OBJurl, '_blank').focus()}
-    className="vacancy-item">
-    <p> <i className="fas fa-pencil-ruler"></i>         {jobTitle}</p>
-    <p> <i className="fas fa-building"></i>             {companyName}</p>
-    <p> <i className="far fa-clock"></i>                {postedDate}</p>
-    <p> <i className="fas fa-file-invoice-dollar"></i>  {shortDesc}</p>
-  </div>
-);
+export const VacancyItem = ({ jobTitle, companyName, shortDesc, postedDate, OBJurl }) => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  console.log(randomColor);
+  return (
+    <div
+      onClick={() => window.open(new URL(OBJurl), '_blank').focus()}
+      className="vacancy-item">
+      <span className="position-top-right-border">                                        Easy Apply</span>
+      <span className="vacancy-item-icon" style={{ backgroundColor: `#${randomColor}` }}>       {companyName.charAt(0)}</span>
+
+      <p className="vacancy-job-title">    <i className="fas fa-pencil-ruler"></i>        {jobTitle}</p>
+      <p className="vacancy-compamy-name"> <i className="fas fa-building"></i>            {companyName}</p>
+      <p className="vacancy-date">         <i className="far fa-clock"></i>               {postedDate}</p>
+      <p className="vacancy-description">  <i className="fas fa-file-invoice-dollar"></i> {shortDesc}</p>
+    </div>
+  )
+};
 
 
 VacancyItem.propTypes = {
